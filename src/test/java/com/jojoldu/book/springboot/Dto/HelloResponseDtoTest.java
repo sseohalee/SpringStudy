d170918326f66714a1dafad60907f8c820d8a4b7
SeohaLee <seohalee0131@gmail.com>
2022-04-28 23:16:42 +0900

    롬복 이용 Dto

---
+++ src/test/java/com/jojoldu/book/springboot/Dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.jojoldu.book.springboot.Dto;
+
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void lombok_test(){
+        //given
+        String name="test";
+        int amount=1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
