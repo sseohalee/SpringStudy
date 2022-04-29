a636f5614d599c0751e87803afa6196a6d1cec4a
SeohaLee <seohalee0131@gmail.com>
2022-04-29 17:50:07 +0900

    롬복 이용 테스트 코드 추가

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import  org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //JSON을 반환하는 컨트롤러로 만들어줌
@@ -9,5 +11,10 @@ public class HelloController {
     @GetMapping("/hello") //HTTP 메소드인 Get의 요청을 받을 수 있는 API로 만들어줆. /hello로 요청이 오면 문자열 hello를 반환하는 기능을 가짐.
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
