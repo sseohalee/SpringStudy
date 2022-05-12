3513043528d4bc6d44c6db259b50acc0c0a910ec
SeohaLee <seohalee0131@gmail.com>
2022-05-12 17:00:18 +0900

    JPA Auditing 활성화 추가

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,7 +2,9 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //JPA Auditing 활성화
 @SpringBootApplication //스프링부트 자동 설정, Bean 읽기와 생성 자동 설정
 public class Application { //프로젝트 메인 클래스
     public static void main(String[] args){
